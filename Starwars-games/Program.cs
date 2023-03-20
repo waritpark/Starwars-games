9037d458385133d40f8b0e2e655f7de1261e1c40
arthur lafarge <lafarge21@hotmail.fr>
2023-03-20 15:41:25 +0100

    feat: modif titre couleur

--- Starwars-games/Program.cs
+++ Starwars-games/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine("A starwars game !");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+// string monTitre = "Starwars game !".ToUpper();
+var monTitre = "Starwars game !".ToUpper();
+
+Console.WriteLine(monTitre);
 Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("Hello world !");
